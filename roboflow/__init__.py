45014650b57c5de9534b95bbd84d27b51d8c2d69
Palash Shah <35114859+Palashio@users.noreply.github.com>
2021-09-03 21:25:47 -0400

    project function() works, although class still needs to be changed

--- roboflow/__init__.py
+++ roboflow/__init__.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+import pprint
 
 import requests
 
@@ -39,28 +40,22 @@ class Roboflow():
 
         return list_projects
 
-    def load(self, dataset_slug):
-        # Get info about dataset being loaded
-        dataset_info = requests.get(
-            API_URL + "/dataset/" + dataset_slug + "?access_token=" + self.access_token)
+    def project(self, dataset_slug):
+
+        dataset_info = requests.get(API_URL + "/" + self.current_workspace + "/" + dataset_slug + "?api_key=" + self.api_key)
+
         # Throw error if dataset isn't valid/user doesn't have permissions to access the dataset
         if dataset_info.status_code != 200:
             raise RuntimeError(dataset_info.text)
-        # Turn dataset info into a json format otherwise
-        dataset_info = dataset_info.json()
-        # Get version info (i.e. version names + numbers)
-        version_info = requests.get(
-            API_URL + "/versions/dataset/" + dataset_slug + "?access_token=" + self.access_token)
-        # Throw error if dataset isn't valid/user doesn't have permissions to access the dataset
-        if version_info.status_code != 200:
-            raise RuntimeError(version_info.text)
-        # Turn dataset version info into a json format otherwise
-        version_info = version_info.json()
-        # Return a project object
-        return Project(self.api_key, dataset_info['id'], dataset_info['type'], version_info['versions'],
-                       self.access_token, self.publishable_key)
+        dataset_info = dataset_info.json()['project']
+
+        return Project(self.api_key, dataset_info['id'], dataset_info['type'], dataset_info['versions'])
 
     def __str__(self):
         json_value = {'api_key': self.api_key,
                       'workspace': self.workspace}
         return json.dumps(json_value, indent=2)
+
+
+rf = auth("FuVGT9Nd8WdzDza6f6qd")
+rf.project("chess-sample-sgdxp")
