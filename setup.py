0c9416c0ec3717e5ec5b92e059288efa7f3806dc
Palash Shah <35114859+Palashio@users.noreply.github.com>
2021-09-15 14:22:28 -0400

    pip package changes

--- setup.py
+++ setup.py
@@ -4,19 +4,32 @@ from setuptools import find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
     name="roboflow",  # Replace with your own username
-    version="0.1.0",
-    author="Roboflow Team",
-    author_email="palash@roboflow.com",
-    description="Pip package for Roboflow.",
+    version="0.1.2",
+    author="Palash Shah",
+    author_email="ps9cmk@virginia.edu",
+    description="Ergonomic machine learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/roboflow-ai/roboflow-python",
-    install_requires=requirements,
+    url="https://github.com/Palashio/libra",
+    install_requires=[
+        "certifi==2021.5.30",
+        "chardet==4.0.0",
+        "cycler==0.10.0",
+        "idna==2.10",
+        "kiwisolver==1.3.1",
+        "matplotlib",
+        "numpy==1.19.5",
+        "opencv-python==4.5.3.56",
+        "pillow",
+        "pyparsing==2.4.7",
+        "python-dateutil",
+        "python-dotenv",
+        "requests==2.25.1",
+        "six",
+        "urllib3==1.26.6",
+    ],
     packages=find_packages(exclude=('tests',)),
     classifiers=[
         "Programming Language :: Python :: 3",
