ed04caba16e7c81a8a5b36c2884b4d9e6b248918
Palash Shah <35114859+Palashio@users.noreply.github.com>
2021-09-15 17:05:48 -0400

    workspace unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,59 @@
 import os
 import roboflow
+from roboflow.core.project import Project
+import unittest
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
+def make_orderer():
+    order = {}
+
+    def ordered(f):
+        order[f.__name__] = len(order)
+        return f
+
+    def compare(a, b):
+        return [1, -1][order[a] < order[b]]
+
+    return ordered, compare
+
+
+ordered, compare = make_orderer()
+unittest.defaultTestLoader.sortTestMethodsUsing = compare
+
+
+class TestQueries(unittest.TestCase):
+    rf = roboflow.Roboflow(api_key="FuVGT9Nd8WdzDza6f6qd")
+    workspace = rf.workspace("palash-shah")
+    """
+    TEST QUERIES
+
+    Tests some queries in queries.py
+    """
+
+    # Tests whether regression_ann_query works without errors, and creates a key in models dictionary
+    @ordered
+    def test_workspace_fields(self):
+        self.assertTrue(isinstance(self.workspace.name, str))
+        self.assertTrue(isinstance(self.workspace.name, str))
+        self.assertTrue(isinstance(self.workspace.project_list, list))
+        self.assertTrue(isinstance(self.workspace.members, int))
+
+    @ordered
+    def test_workspace_methods(self):
+        print_projects = self.workspace.list_projects()
+        project_array = self.workspace.projects()
+        project_obj = self.workspace.project("test-chess")
+
+        self.assertIsNone(print_projects)
+        self.assertTrue(isinstance(project_array, list))
+        self.assertTrue(isinstance(project_obj, Project))
+
+
+
 if __name__ == '__main__':
-    #Authenticate roboflow
-    rflow = roboflow.Roboflow(api_key="API_KEY")
-    #Retrieve specific project
-    project = rflow.project("PROJECT_NAME")
-    #List all versions
-    project.versions()
-    #Get first version
-    project.version("1")
-    #Retrieve first model version
-    model = project.version("1").model
-    #Predict on a certain image
-    prediction = model.predict("IMAGE_NAME")
-    #Plot the prediction
-    prediction.plot()
-    #Save the prediction as an image
-    prediction.save(output_path='classify.jpg')
+    unittest.main()
+
