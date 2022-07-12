f87d96cff617e049ee245dab432b95d482d2260f
Jacobsolawetz <jacobsolawetz@gmail.com>
2022-07-12 18:18:21 -0400

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="roboflow",  # Replace with your own username
-    version="0.2.7",
+    version="0.2.8",
     author="Roboflow",
     author_email="jacob@roboflow.com",
     description="python client for the Roboflow application",
