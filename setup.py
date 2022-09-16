d5a7169285c25858cbd53ca14c10530cf58da50d
Derek Brooks <derek@broox.com>
2022-09-16 12:28:03 -0500

    Refactor tests and fix inability to upload valid image formats with capitalized extensions (#51)
    * Refactor tests and fix #50 - inability to upload valid image formats with capitalized extensions
    
    * mock API responses.
    
    * linted
    
    * try to use the github event repository for the github action checkout

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
     ],
     packages=find_packages(exclude=("tests",)),
     extras_require={
-        "dev": ["flake8", "black==22.3.0", "isort"],
+        "dev": ["flake8", "black==22.3.0", "isort", "responses"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
