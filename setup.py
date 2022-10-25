7443284c81aae776acccff4d526a8b394ef24506
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2022-10-25 17:58:20 +0200

    minor changes

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 from setuptools import find_packages
 import re
 
-with open("./src/__init__.py", 'r') as f:
+with open("./src/__init__.py", "r") as f:
     content = f.read()
     # from https://www.py4u.net/discuss/139845
     version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
-    
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -44,7 +44,7 @@ setuptools.setup(
     ],
     packages=find_packages(exclude=("tests",)),
     extras_require={
-        "dev": ["flake8", "black==22.3.0", "isort", "responses","twine", "wheel"],
+        "dev": ["flake8", "black==22.3.0", "isort", "responses", "twine", "wheel"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
