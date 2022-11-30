2dcd7a177be625e137c56d6c72804a7e6f284bb3
Francesco Saverio Zuppichini <zuppif@usi.ch>
2022-11-30 18:26:30 +0100

    Update __init__.py

--- roboflow/__init__.py
+++ roboflow/__init__.py
@@ -7,7 +7,7 @@ from roboflow.config import API_URL, APP_URL, DEMO_KEYS
 from roboflow.core.project import Project
 from roboflow.core.workspace import Workspace
 
-__version__ = "0.2.17"
+__version__ = "0.2.20"
 
 
 def check_key(api_key, model, notebook):
