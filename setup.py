94af34e88f63cc4454a8276a6f355f8a854da69c
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2023-01-26 13:30:51 +0100

    using requirements.txt for install_requires in setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,11 @@ with open("./roboflow/__init__.py", "r") as f:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# we are using the packages in `requirements.txt` for now, 
+# not 100% ideal but will do
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read().split('\n')
+
 setuptools.setup(
     name="roboflow",
     version=version,
@@ -20,28 +25,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/roboflow-ai/roboflow-python",
-    install_requires=[
-        "certifi==2022.12.7",
-        "chardet==4.0.0",
-        "cycler==0.10.0",
-        "glob2",
-        "idna==2.10",
-        "kiwisolver>=1.3.1",
-        "matplotlib",
-        "numpy>=1.18.5",
-        "opencv-python-headless>=4.5.1.48",
-        "Pillow>=7.1.2",
-        "pyparsing==2.4.7",
-        "python-dateutil",
-        "python-dotenv",
-        "requests",
-        "requests_toolbelt",
-        "six",
-        "urllib3==1.26.6",
-        "tqdm>=4.41.0",
-        "PyYAML>=5.3.1",
-        "wget",
-    ],
+    install_requires=install_requires,
     packages=find_packages(exclude=("tests",)),
     extras_require={
         "dev": ["flake8", "black==22.3.0", "isort", "responses", "twine", "wheel"],
