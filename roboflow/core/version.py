883756828da7f81af3221f5bf983bb5312479794
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2023-01-27 16:33:09 +0100

    minor changes

--- roboflow/core/version.py
+++ roboflow/core/version.py
@@ -166,7 +166,7 @@ class Version:
                     [("ultralytics", "<=", "8.0.20")]
                 )
             except ImportError as e:
-                print("[WARNING] we noticed you are downloading a `yolov8` datasets but you don't have `ultralytics` installed. You will need to install it to call `.deploy` later, you can use: `pip install ultralytics<=8.0.20`.")
+                print("[WARNING] we noticed you are downloading a `yolov8` datasets but you don't have `ultralytics` installed. Roboflow `.deploy` supports only models trained with `ultralytics<=8.0.20`, to intall it `pip install ultralytics<=8.0.20`.")
                 # silently fail
                 pass
 
