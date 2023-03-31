94fad7df94005a9e6b957d4830d6b1eb855bb3f6
peterxcli <54034701+peterxcli@users.noreply.github.com>
2023-04-01 01:07:35 +0800

    fix env variable for windows
     File "C:\Users\<username>\.conda\envs\ML\lib\site-packages\roboflow\config.py", line 20, in get_conditional_configuration_variable
        default=os.getenv("HOME") + "/.config/roboflow/config.json",
    TypeError: unsupported operand type(s) for +: 'NoneType' and 'str'

--- roboflow/config.py
+++ roboflow/config.py
@@ -17,7 +17,7 @@ def get_conditional_configuration_variable(key, default):
     # default configuration location
     conf_location = os.getenv(
         "ROBOFLOW_CONFIG_DIR",
-        default=os.getenv("HOME") + "/.config/roboflow/config.json",
+        default=os.path.expanduser('~') + "/.config/roboflow/config.json",
     )
 
     # read config file for roboflow if logged in from python or CLI
