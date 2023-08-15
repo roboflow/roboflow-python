8185b493ca2571fcd390cf1dd780007ef052092d
Tony Lampada <tonylampada@gmail.com>
2023-08-15 17:19:05 -0300

    upload big datasets more robustly

--- roboflow/util/general.py
+++ roboflow/util/general.py
@@ -5,3 +5,18 @@ def write_line(line):
     sys.stdout.write("\r" + line)
     sys.stdout.write("\n")
     sys.stdout.flush()
+
+def retry(max_retries, retry_on, func, *args, **kwargs):
+    if not retry_on:
+        retry_on = (Exception, )
+    retries = 0
+    while retries <= max_retries:
+        try:
+            return func(*args, **kwargs)
+        except BaseException as e:
+            if isinstance(e, retry_on):
+                retries += 1
+                if retries > max_retries:
+                    raise
+            else:
+                raise
