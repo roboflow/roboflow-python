d6a01ccddbfdd1e9e29f7cd6ea853f6d763683f3
Tony Lampada <tonylampada@gmail.com>
2024-03-07 13:33:48 +0000

    goodbye RF_API_KEY, hello ROBOFLOW_API_KEY

--- roboflow/config.py
+++ roboflow/config.py
@@ -77,8 +77,8 @@ RF_WORKSPACES = get_conditional_configuration_variable("workspaces", default={})
 
 
 def load_roboflow_api_key(workspace_url=None):
-    if os.getenv("RF_API_KEY") is not None:
-        return os.getenv("RF_API_KEY")
+    if os.getenv("ROBOFLOW_API_KEY") is not None:
+        return os.getenv("ROBOFLOW_API_KEY")
     RF_WORKSPACES = get_conditional_configuration_variable("workspaces", default={})
     workspaces_by_url = {w["url"]: w for w in RF_WORKSPACES.values()}
     default_workspace_url = get_conditional_configuration_variable("RF_WORKSPACE", default=None)
