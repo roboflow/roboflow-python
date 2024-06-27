a7ab284a576b9198179216f92700bdd109184541
Iuri de Silvio <iuri@roboflow.com>
2024-06-27 18:06:23 +0200

    Fix `GazeModel`

--- roboflow/models/gaze.py
+++ roboflow/models/gaze.py
@@ -6,11 +6,12 @@ class GazeModel(InferenceModel):
     Run inference on a gaze detection model, hosted on Roboflow.
     """
 
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, version_id: str):
         """
         Initialize a CLIP model.
 
         Args:
             api_key: Your Roboflow API key.
+            version_id (str): the ID of the dataset version to use for inference
         """
-        super().__init__(api_key=api_key)
+        super().__init__(api_key=api_key, version_id=version_id)
