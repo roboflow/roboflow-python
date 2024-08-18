33572c25e270194f57c624b228c42360c5da1b21
Leandro Rosemberg <leandro.rosemberg@buser.com.br>
2024-08-18 17:09:18 -0300

    It is not good to have a global response

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,8 +1,20 @@
-from tests import RoboflowTest
+import responses
+
+from roboflow import API_URL
+from roboflow.config import DEFAULT_BATCH_NAME
+from tests import RoboflowTest, PROJECT_NAME, ROBOFLOW_API_KEY
 
 
 class TestProject(RoboflowTest):
     def test_check_valid_image_with_accepted_formats(self):
+        # Mock dataset upload
+        responses.add(
+            responses.POST,
+            f"{API_URL}/dataset/{PROJECT_NAME}/upload?api_key={ROBOFLOW_API_KEY}" f"&batch={DEFAULT_BATCH_NAME}",
+            json={"duplicate": True, "id": "hbALkCFdNr9rssgOUXug"},
+            status=200,
+        )
+
         images_to_test = [
             "rabbit.JPG",
             "rabbit2.jpg",
@@ -14,6 +26,14 @@ class TestProject(RoboflowTest):
             self.assertTrue(self.project.check_valid_image(f"tests/images/{image}"))
 
     def test_check_valid_image_with_unaccepted_formats(self):
+        # Mock dataset upload
+        responses.add(
+            responses.POST,
+            f"{API_URL}/dataset/{PROJECT_NAME}/upload?api_key={ROBOFLOW_API_KEY}" f"&batch={DEFAULT_BATCH_NAME}",
+            json={"duplicate": True, "id": "hbALkCFdNr9rssgOUXug"},
+            status=200,
+        )
+
         images_to_test = [
             "sky-rabbit.gif",
             "sky-rabbit.heic",
