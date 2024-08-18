875addd0dc84b4ab63c7ec4620c2d048f2caf5e9
Leandro Rosemberg <leandro.rosemberg@buser.com.br>
2024-08-18 18:25:21 -0300

    Tests for image upload error, raising a UploadImageError

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,7 +2,8 @@ import responses
 
 from roboflow import API_URL
 from roboflow.config import DEFAULT_BATCH_NAME
-from tests import PROJECT_NAME, ROBOFLOW_API_KEY, RoboflowTest
+from roboflow.core.exceptions import UploadImageError
+from tests import RoboflowTest, PROJECT_NAME, ROBOFLOW_API_KEY
 
 
 class TestProject(RoboflowTest):
@@ -41,3 +42,23 @@ class TestProject(RoboflowTest):
 
         for image in images_to_test:
             self.assertFalse(self.project.check_valid_image(f"tests/images/{image}"))
+
+    def test_upload_image_that_already_is_annotated_raises_upload_image_error(self):
+        responses.add(
+            responses.POST,
+            f"{API_URL}/dataset/{PROJECT_NAME}/upload?api_key={ROBOFLOW_API_KEY}" f"&batch={DEFAULT_BATCH_NAME}",
+            json={
+                "message": "Image was already annotated.",
+                "type": "InvalidImageException",
+                "hint": "This image was already annotated; to overwrite the annotation, pass overwrite=true...",
+              },
+            status=200,
+        )
+
+        with self.assertRaises(UploadImageError) as error:
+            self.project.upload(
+                "tests/images/rabbit.JPG",
+                annotation_path="tests/annotations/valid_annotation.json",
+            )
+
+        self.assertEquals(str(error.exception), "Error uploading image: Image was already annotated.")
