f61ee80d4ef820809ec88af8acf13064ec1d982c
PacificDou <dsy.dlut@gmail.com>
2024-08-21 16:05:58 +0000

    setup positional and optional params properly

--- roboflow/adapters/deploymentapi.py
+++ roboflow/adapters/deploymentapi.py
@@ -9,55 +9,50 @@ class DeploymentApiError(Exception):
 
 def add_deployment(api_key, machine_type, duration, delete_on_expiration, deployment_name, inference_version):
     url = f"{DEDICATED_DEPLOYMENT_URL}/add"
-    response = requests.post(
-        url,
-        json={
+    params = {
             "api_key": api_key,
             # "security_level": security_level,
-            "machine_type": machine_type,
             "duration": duration,
             "delete_on_expiration": delete_on_expiration,
             "deployment_name": deployment_name,
             "inference_version": inference_version,
-        },
-    )
+            }
+    if machine_type is not None:
+        params["machine_type"] = machine_type
+    response = requests.post(url, json = params)
     if response.status_code != 200:
-        raise DeploymentApiError(f"{response.status_code}: {response.text}")
-    result = response.json()
-    return result
+        return response.status_code, response.text
+    return response.status_code, response.json()
 
 
-def get_deployment(api_key, deployment_id):
-    url = f"{DEDICATED_DEPLOYMENT_URL}/get"
-    response = requests.get(url, json={"api_key": api_key, "deployment_id": deployment_id})
+def get_deployment(api_key, deployment_name):
+    url = f"{DEDICATED_DEPLOYMENT_URL}/get?api_key={api_key}&deployment_name={deployment_name}"
+    response = requests.get(url)
     if response.status_code != 200:
-        raise DeploymentApiError(f"{response.status_code}: {response.text}")
-    result = response.json()
-    return result
+        return response.status_code, response.text
+    return response.status_code, response.json()
 
 
 def list_deployment(api_key):
-    url = f"{DEDICATED_DEPLOYMENT_URL}/list"
-    response = requests.get(url, json={"api_key": api_key})
+    url = f"{DEDICATED_DEPLOYMENT_URL}/list?api_key={api_key}"
+    response = requests.get(url)
     if response.status_code != 200:
-        raise DeploymentApiError(f"{response.status_code}: {response.text}")
-    result = response.json()
-    return result
+        return response.status_code, response.text
+    return response.status_code, response.json()
 
 
-def delete_deployment(api_key, deployment_id):
+def delete_deployment(api_key, deployment_name):
     url = f"{DEDICATED_DEPLOYMENT_URL}/delete"
-    response = requests.post(url, json={"api_key": api_key, "deployment_id": deployment_id})
+    response = requests.post(url, json={"api_key": api_key, "deployment_name": deployment_name})
     if response.status_code != 200:
-        raise DeploymentApiError(f"{response.status_code}: {response.text}")
-    result = response.json()
-    return result
+        return response.status_code, response.text
+    return response.status_code, response.json()
 
 
 def list_machine_types(api_key):
-    url = f"{DEDICATED_DEPLOYMENT_URL}/machine_types"
-    response = requests.get(url, json={"api_key": api_key})
+    url = f"{DEDICATED_DEPLOYMENT_URL}/machine_types?api_key={api_key}"
+    response = requests.get(url)
     if response.status_code != 200:
-        raise DeploymentApiError(f"{response.status_code}: {response.text}")
-    result = response.json()
-    return result
+        return response.status_code, response.text
+    return response.status_code, response.json()
+
