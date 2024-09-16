53c9b216ef0e1ab95406d3044829dd2f36e55c60
Tony Lampada <tonylampada@gmail.com>
2024-09-16 10:17:15 -0300

    ignore lint

--- tests/util/test_folderparser.py
+++ tests/util/test_folderparser.py
@@ -58,8 +58,10 @@ class TestFolderParser(unittest.TestCase):
         testImagePath = "/dataset/de48275e1ff70fab78bee31e09fc896d_png.rf.01a97b1ad053aa1e6525ac0451cee8b7.jpg"
         testImage = [i for i in parsed["images"] if i["file"] == testImagePath][0]
         assert testImage["annotationfile"]["name"] == "annotation.jsonl"
+        # fmt: off
         expected = """{"image": "de48275e1ff70fab78bee31e09fc896d_png.rf.01a97b1ad053aa1e6525ac0451cee8b7.jpg", "prefix": "Which sector had the highest ROI in 2013?", "suffix": "Retail"}
 {"image": "de48275e1ff70fab78bee31e09fc896d_png.rf.01a97b1ad053aa1e6525ac0451cee8b7.jpg", "prefix": "Which sector had the highest ROI in 2014?", "suffix": "Electronics"}"""
+        # fmt: on
         assert testImage["annotationfile"]["rawText"] == expected
 
 
