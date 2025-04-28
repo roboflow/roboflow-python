33b289015a28095c429134fefe49c987814f38af
Iuri de Silvio <iuri@roboflow.com>
2025-04-28 14:28:54 -0500

    Normalize Windows style paths to parse folder (#376)

--- roboflow/__init__.py
+++ roboflow/__init__.py
@@ -15,7 +15,7 @@ from roboflow.core.workspace import Workspace
 from roboflow.models import CLIPModel, GazeModel  # noqa: F401
 from roboflow.util.general import write_line
 
-__version__ = "1.1.61"
+__version__ = "1.1.62"
 
 
 def check_key(api_key, model, notebook, num_retries=0):
